8f1fd4a364c1e2945bf27050792058133a32b529
Campbell Forsyth <camsh69@gmail.com>
2021-11-18 14:40:03 +0000

    fourth commit

--- start_point/src/friends.py
+++ start_point/src/friends.py
@@ -3,3 +3,10 @@ def get_name(person):
 
 def get_favourite_tv_show(person):
     return person["favourites"]["tv_show"]
+
+def likes_to_eat(person, food):
+    for snack in person["favourites"]["snacks"]:
+        if snack.lower() == food.lower():
+            return True
+
+    return False
